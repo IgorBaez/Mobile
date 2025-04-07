702bc53816dfa2cb03674abb27af730976ef3764
IgorBaez <igorbaez123@gmail.com>
2025-04-07 17:31:05 -0400

    att

--- aulas/App.js
+++ aulas/App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <AntDesign name="dingding" size={24} color="black" />
+      
       <StatusBar style="auto" />
     </View>
   );
